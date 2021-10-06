1c30c7e8608138c2d8858d4f8b9eefd7101a2cd4
blasingim.levi <blasingim.levi@vashonsd.org>
2021-10-06 10:47:07 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(squareOf(4));
+    }
+
+
+
+    public static int squareOf(int num) {
+        return(num * num);
+    }
+}
